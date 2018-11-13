78f2a66059020cc843a1dff14a14ff019e2721c5
bobro-krab <bobro-krab@bk.ru>
2018-11-13 10:20:45 +0700

    Implement RayCast template with correct color output to image

--- src/main.cpp
+++ src/main.cpp
@@ -45,23 +45,23 @@ void WriteImage(image_u32 Image,  char *Filename) {
 
 }
 
-int main(int argc, char **argv)
+v3 RayCast(v3 Origin, v3 Direction, world *World) {
+    v3 Result = {};
+    Result.r = 0.5;
+    Result.g = 0;
+    Result.b = 0;
+    return Result;
+}
+
+int main()
 {
-    image_u32 Image = AllocateImage(1280, 720);
-
-    u32 *Out = Image.Pixels;
-    for (u32 y = 0; y < Image.Height; y++) {
-        for (u32 x = 0; x < Image.Width; x++) {
-            *Out++ = (y < 32) ?  0xffff0000: 0xff0000ff ;
-        }
-    }
-
-    WriteImage(Image, "test.bmp");
     material Materials[2] = {};
     Materials[0].Color = V3(0, 0, 0);
     Materials[1].Color = V3(1, 0, 0);
 
     plane Plane = {};
+    Plane.N = V3(0, 0, 1);
+    Plane.d = 0;
     Plane.MatIndex = 1;
 
     world World = {};
@@ -73,6 +73,39 @@ int main(int argc, char **argv)
 
     World.SphereCount = 0;
     World.Spheres = 0;
+
+    v3 CameraPos = V3(0, 10, 1);
+    v3 CameraZ = NOZ(CameraPos);
+    v3 CameraX = NOZ(Cross(CameraZ, V3(0, 0, 1)));
+    v3 CameraY = NOZ(Cross(CameraZ, CameraX));
+
+    f32 FilmDistanceToCamera = 1.0f;
+    v3 FilmCenter = CameraPos - FilmDistanceToCamera * CameraZ;
+    f32 FilmW = 1.0f;
+    f32 FilmH = 1.0f;
+
+    f32 HalfFilmW = FilmW / 2.0f;
+    f32 HalfFilmH = FilmH / 2.0f;
+
+    image_u32 Image = AllocateImage(1280, 720);
+
+    u32 *Out = Image.Pixels;
+    for (u32 y = 0; y < Image.Height; y++) {
+        f32 FilmY = -1.0f + 2.0f * ((f32)y / (f32)Image.Height);
+        for (u32 x = 0; x < Image.Width; x++) {
+            f32 FilmX = -1.0f + 2.0f * ((f32)x / (f32)Image.Width);
+
+            v3 FilmP = FilmCenter + HalfFilmW * FilmX * CameraX + HalfFilmH * FilmY * CameraY;
+            v3 RayOrigin = CameraPos;
+            v3 RayDirection = NOZ(FilmP - CameraPos);
+
+            v3 Color = RayCast(RayOrigin, RayDirection, &World);
+
+            *Out ++ = BMPPackVector(Color);
+        }
+    }
+
+    WriteImage(Image, "test.bmp");
 
     return 0;
 }
--- src/ray.h
+++ src/ray.h
@@ -1,4 +1,6 @@
+#include <math.h>
 #define internal static
+
 
 typedef char unsigned u8;
 typedef short unsigned u16;
@@ -9,6 +11,29 @@ typedef short s16;
 typedef int s32;
 
 typedef float f32;
+
+union v3 {
+    struct
+    {
+        f32 u, v, _;
+    };
+    struct
+    {
+        f32 x, y, z;
+    };
+    struct
+    {
+        f32 r, g, b;
+    };
+};
+
+v3 V3(f32 x, f32 y, f32 z) {
+    v3 result;
+    result.x = x;
+    result.y = y;
+    result.z = z;
+    return result;
+}
 
 #pragma pack(push, 1)
 struct bitmap_header {
@@ -65,3 +90,71 @@ struct world {
     u32 SphereCount;
     sphere *Spheres;
 };
+
+v3 operator*(f32 multiplyer, v3 A ) {
+    v3 Result = {};
+    Result.x = A.x * multiplyer;
+    Result.y = A.y * multiplyer;
+    Result.z = A.z * multiplyer;
+    return Result;
+};
+
+v3 operator*(v3 A, f32 multiplyer) {
+    return operator*(multiplyer, A);
+}
+
+v3 operator-(v3 A, v3 B ) {
+    v3 Result = {};
+    Result.x = A.x - B.x;
+    Result.y = A.y - B.y;
+    Result.z = A.z - B.z;
+    return Result;
+};
+
+
+v3 operator+(v3 A, v3 B ) {
+    v3 Result = {};
+    Result.x = A.x + B.x;
+    Result.y = A.y + B.y;
+    Result.z = A.z + B.z;
+    return Result;
+};
+
+
+f32 Inner(v3 A, v3 B) {
+    f32 result = A.x * B.x + A.y * B.y + A.z * B.z;
+    return result;
+}
+
+f32 LengthSq(v3 A) {
+    return Inner(A, A);
+}
+
+v3 Cross(v3 A, v3 B) {
+    v3 Result = {};
+
+    Result.x = A.y * B.y + A.z * B.z;
+    Result.y = A.x * B.x + A.z * B.z;
+    Result.z = A.x * B.x + A.y * B.y;
+
+    return Result;
+}
+
+v3 NOZ(v3 A) {
+    v3 Result = {};
+    f32 LenSq = LengthSq(A);
+    if (LenSq > 0.00001 * 0.00001) {
+        Result = A * (1.0f / sqrt(LenSq));
+    }
+    return Result;
+}
+
+u32 BMPPackVector(v3 v) {
+    u32 Result = (
+        (0xFF << 24) |
+        ((u32)(255.0f * v.r) << 16) |
+        ((u32)(255.0f * v.g) << 8) |
+        ((u32)(255.0f * v.b) << 0)
+        );
+    return Result;
+}
